a804ad4c0e7b1a49ba070a4d2c37b7ab2e117c82
jacob-oreilly <jakeor45@gmail.com>
2022-03-15 00:30:07 -0600

    Adding basic function for adding and getting posts

--- group-ride-api/app.py
+++ group-ride-api/app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+
+posts = [
+
+]
 
 @app.route("/")
 def hello_world():
   return "Hello, World!"
+
+@app.route("/posts")
+def get_posts():
+  return jsonify(posts)
+
+
+@app.route("/posts", methods=["POST"])
+def add_post():
+  posts.append(request.get_json())
+  return '', 204
